307089d23eee97c795548b5de9c17ba1e104284d
ThayaTamil <128203916+ThayaTamil@users.noreply.github.com>
2023-03-18 09:30:04 +0530

    Add files via upload

---
+++ database/register.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `users` (
+  `u_id` int(222) NOT NULL,
+  `username` varchar(222) NOT NULL,
+  `firstname` varchar(222) NOT NULL,
+  `lastname` varchar(222) NOT NULL,
+  `email` varchar(222) NOT NULL,
+  `phonenumber` varchar(222) NOT NULL,
+  `password` varchar(222) NOT NULL,
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
---
+++ php/register.php
@@ -0,0 +1,77 @@
+<?php
+
+session_start(); //temp session
+error_reporting(0); // hide undefine index
+include("php\connect.php"); // connection
+if(isset($_POST['submit'] )) //if submit btn is pressed
+{
+     if(empty($_POST['firstname']) ||  //fetching and find if its empty
+   	    empty($_POST['lastname'])|| 
+		empty($_POST['email']) ||  
+		empty($_POST['phonenumber'])||
+		empty($_POST['password'])||
+		empty($_POST['cpassword']) ||
+		empty($_POST['cpassword']))
+		{
+			$message = "All fields must be Required!";
+		}
+	else
+	{
+		//cheching username & email if already present
+	$check_username= mysqli_query($db, "SELECT username FROM users where username = '".$_POST['username']."' ");
+	$check_email = mysqli_query($db, "SELECT email FROM users where email = '".$_POST['email']."' ");
+		
+
+	
+	if($_POST['password'] != $_POST['cpassword']){  //matching passwords
+       	$message = "Password not match";
+    }
+	elseif(strlen($_POST['password']) < 6)  //cal password length
+	{
+		$message = "Password Must be >=6";
+	}
+	elseif(strlen($_POST['phonenumber']) < 10)  //cal phone length
+	{
+		$message = "invalid phone number!";
+	}
+
+    elseif (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) // Validate email address
+    {
+       	$message = "Invalid email address please type a valid email!";
+    }
+	elseif(mysqli_num_rows($check_username) > 0)  //check username
+     {
+    	$message = 'username Already exists!';
+     }
+	elseif(mysqli_num_rows($check_email) > 0) //check email
+     {
+    	$message = 'Email Already exists!';
+     }
+	else{
+       
+	 //inserting values into db
+	$mql = "INSERT INTO users(username,firstname,lastname,email,phonenumber,password) VALUES('".$_POST['username']."','".$_POST['firstname']."','".$_POST['lastname']."','".$_POST['email']."','".$_POST['phonenumber']."','".md5($_POST['password'])."',')";
+	mysqli_query($db, $mql);
+		$success = "Account Created successfully! <p>You will be redirected in <span id='counter'>5</span> second(s).</p>
+														<script type='text/javascript'>
+														function countdown() {
+															var i = document.getElementById('counter');
+															if (parseInt(i.innerHTML)<=0) {
+																location.href = 'php/login.php';
+															}
+															i.innerHTML = parseInt(i.innerHTML)-1;
+														}
+														setInterval(function(){ countdown(); },1000);
+														</script>'";
+		
+		
+		
+		
+		 header("refresh:5;url=login.html"); // redireted once inserted success
+    }
+	}
+
+}
+
+
+?>
